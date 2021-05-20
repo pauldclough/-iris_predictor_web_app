ddb36f425a0ca8ff4d345b8a518b420ce8347fcc
Paul Clough <50335409+pauldclough@users.noreply.github.com>
2021-05-20 15:27:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -64,6 +64,8 @@ def prediction():
 	results = return_prediction(flower_model, flower_scaler, content)
 	return render_template('prediction.html', results=results)
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    serve(app,host='0.0.0.0', port=5000)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
